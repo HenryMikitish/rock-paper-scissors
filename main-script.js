f22589c1b8eadbb7aabc19b7a1d387189aebab04
HenryMikitish <henry.mikitish@gmail.com>
2023-01-18 16:19:33 -0800

    Set up a new, basic structure to play as a button-press game

--- main-script.js
+++ main-script.js
@@ -1,3 +1,12 @@
+const yourScoreBoard = document.querySelector('.yourscore');
+const oppScoreBoard = document.querySelector('.oppscore');
+let yourScore = 0;
+let oppScore = 0;
+choice = [];
+compchoice = [];
+const yourPlay = document.querySelector('.yourplay');
+const oppPlay = document.querySelector('.oppplay');
+
 //Possible plays for the computer
 const computerPlays = [
     'grass',
@@ -8,89 +17,49 @@ const computerPlays = [
 //Randomizes the computer's play
 function randomElement(myArray) {
     let index = Math.floor(Math.random() * myArray.length); 
-	return myArray[index];
-};
-
-//Plays a round of the game
-function playRound() {
-
-    //Counts the wins and losses of the player
-    const wins = ['Start'];
-    const losses = ['Start'];
-
-    for (i = 1; i = 1; i) {
-
-        if(wins.length == 4) {
-            alert('You win the best-of-five!');
-            throw 'exit';
-        }
-        else if(losses.length == 4) {
-            alert('You lose the best-of-five!')
-            throw 'exit';
-        }
-        //The actual round of the game that plays if no one has won yet
-        else {
-            let userPlay = pullUser();
-            let computerPlay =pullComputer();
-
-            if(userPlay == computerPlay) {
-                alert('You and the computer picked the same Type, so it\'s a draw!');
-            }
-            else if(
-                userPlay == 'grass' && computerPlay == 'fire' ||
-                userPlay == 'fire' && computerPlay == 'water' ||
-                userPlay == 'water' && computerPlay == 'grass'
-            ) {
-                alert('The computer picked the Type that beats you, so you lose!');
-                losses.push('L');
-            }
-            else if(
-                userPlay == 'grass' && computerPlay == 'water' ||
-                userPlay == 'fire' && computerPlay == 'grass' ||
-                userPlay == 'water' && computerPlay == 'fire'
-            ) {
-                alert('The computer picked the Type weak to you, so you win!')
-                wins.push('W');
-            }
-            else {
-                alert('Remember, Grass beats Water, Fire beats Grass, and Water beats Fire!');
-                throw 'exit';
-            }
-        }   
-    } 
+    return myArray[index];
 };
 
 //Gets the computer's play from a randomizing function + an array
 function pullComputer() {
-    return randomElement(computerPlays);
+    compchoice.push(randomElement(computerPlays));
 };
 
-//Converts the user console input into item to be used by playRound
-function pullUser() {
-    let x = prompt('Pick your Type: Grass, Fire, or Water! Win a best-of-five vs. the computer, who will pick a Type, too, a la "Rock Paper Scissors." Let\'s play!');
+gbtn.addEventListener('click', function() {
+    choice.push('grass');
+    pullComputer();
+    compareResults();
+});
 
-    //Handles everything but cancels, etc.
-    if (x != null) {
-        let xcorrect = x.trim().toLowerCase();
 
-        if(xcorrect == 'grass') {
-            return 'grass';
-        }
-        else if(xcorrect == 'fire') {
-            return 'fire';
-        }
-        else if(xcorrect == 'water') {
-            return 'water';
-        }
-        //The playRound() function handles the null
-        else {
-            return null;
-        }
-    }
-    else {
-        alert('Remember, Grass beats Water, Fire beats Grass, and Water beats Fire!');
-        throw 'exit';
-    }
-};
+fbtn.addEventListener('click', function() {
+    choice.push('fire');
+    pullComputer();
+    compareResults();
+});
 
-let ans = playRound();
+wbtn.addEventListener('click', function() {
+    choice.push('water');
+    pullComputer();
+    compareResults();
+});
+
+function compareResults() {
+    if (choice[0] == 'grass') {yourPlay.textContent = 'Go, Grass!'};
+    if (choice[0] == 'fire') {yourPlay.textContent = 'Go, Fire!'};
+    if (choice[0] == 'water') {yourPlay.textContent = 'Go, Water!'};
+    if (compchoice[0] == 'grass') {oppPlay.textContent = 'Go, Grass!'};
+    if (compchoice[0] == 'fire') {oppPlay.textContent = 'Go, Fire!'};
+    if (compchoice[0] == 'water') {oppPlay.textContent = 'Go, Water!'};
+
+    if (choice[0] == 'grass') {yourScore++};
+    if (choice[0] == 'fire') {yourScore++};
+    if (compchoice[0] == 'grass') {oppScore++};
+    if (compchoice[0] == 'fire') {oppScore++};
+    if (compchoice[0] == 'water') {oppScore++};
+    choice.splice(0, choice.length);
+    compchoice.splice(0, compchoice.length);
+    yourScoreBoard.textContent = yourScore;
+    oppScoreBoard.textContent = oppScore;
+
+}
